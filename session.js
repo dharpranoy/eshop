eed18d9d62166a8df2055d90b2312d5c179eb9dd
localp <tutor.pnd19@gmail.com>
2022-04-25 23:00:30 +0530

    added extra features

--- session.js
+++ session.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const mysql = require('mysql')
 const path = require('path')
+const fs = require('fs')
 const nodemailer = require('nodemailer')
 const bodyparser = require('body-parser')
 const upl = require('multer')()
+const PDFDocument = require('pdfkit')
 const cookieParser = require('cookie-parser')
 const session = require('express-session')
 let app = express()
@@ -13,7 +15,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(bodyparser.urlencoded({extended:true}))
 app.use(upl.array())
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname+'/public')))	
 app.use(session({
 	secret:'Admin@fedora35',
 	resave:true,
@@ -80,7 +82,7 @@ app.get('/home',(req,res)=>{
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<title>kart home</title>
-		<link ref='icon' href='icon.ico' type='image/x-icon'>
+		<link ref='icon' href='src/icon.ico' type='image/x-icon'>
 		<script type="text/javascript">
 			handle=(f_url)=>{
 				fetch('/'+f_url,{
@@ -94,10 +96,22 @@ app.get('/home',(req,res)=>{
 							case 'suggest': 
 								for (const co of cons){
 									let par = document.getElementById('hover')
+									let cont = document.createElement('div')
+									let tag = document.createElement('h3')
+									tag.innerHTML=co.pname
+									cont.appendChild(tag)
+									let icon = document.createElement('img')
+									icon.setAttribute('src',co.imgpath)
+									icon.setAttribute('height','110px')
+									icon.setAttribute('width','100px')
+									cont.appendChild(icon)
 									let chl = document.createElement('a')
-									chl.innerHTML = co.pname
+									let but = document.createElement('button')
+									but.innerHTML="View"
+									chl.appendChild(but)
 									chl.setAttribute('href','/desc?pid='+co.pcode)
-									par.appendChild(chl)
+									cont.appendChild(chl)
+									par.appendChild(cont)
 								}
 								break
 							case 'products':
@@ -114,14 +128,16 @@ app.get('/home',(req,res)=>{
 											let tba = document.createElement('table')
 											let trow = document.createElement('tr')
 											let tdata = document.createElement('td')
+											tdata.innerHTML="Name : "+co.prname+"<br>"+" id : "+co.prid+"<br>"
 											let chl = document.createElement('a')
-											chl.innerHTML="Name : "+co.prname+" id : "+co.prid
+											chl.innerHTML="<button>"+"View"+"</button>"
 											chl.setAttribute('href','/desc?pid='+co.prid)
 											tdata.appendChild(chl)
 											
 											let rem = document.createElement('button')
 											rem.setAttribute('type','button')
 											rem.innerHTML="Remove item"
+											rem.setAttribute
 											rem.addEventListener('click',()=>{
 												
 											})
@@ -147,8 +163,15 @@ app.get('/home',(req,res)=>{
 									chl.setAttribute('href','/desc?pid='+co.orderid)
 									tdata.appendChild(chl)
 									let pe = document.createElement('p')
-									pe.innerHTML="Shipping address : "+co.shipping+"Price : "+co.ocost+"/-"
+									pe.innerHTML="Shipping address : "+co.shipping+"<br>"+"Price : "+co.ocost+"/-"
+
+									let invo=document.createElement('button')
+									invo.setAttribute('type','button')
+									
+									invo.innerHTML='Download Invoice'
+									invo.addEventListener('click',down(co.orderid))
 									tdata.appendChild(pe)
+									tdata.appendChild(invo)
 									trow.appendChild(tdata)
 									tba.appendChild(trow)
 									par.appendChild(tba)
@@ -160,73 +183,140 @@ app.get('/home',(req,res)=>{
 					
 				})
 			}
+			down=(ytp)=>{
+				let obj={
+					data:ytp
+				}
+				fetch('/invoice',{
+					method:'post',
+					body:JSON.stringify(obj),
+					headers:{'Content-type':'application/json'}
+				})
+				
+			}
 		</script>
 		<style type='text/css'>
 			body{
-				background-color:#472532;
+				background-color:white;
+				width:auto;
+				margin:auto;
 			}
-			#top-strip>ul{
+			#top-strip{
+				display:flex;
+				justify-content:space-around;
+				background-color:#472532;	
+			}
+			#navlinks>ul{
 				list-style:none;
 				display:flex;
-				justify-content:space-between;
+				justify-content:space-evenly;
 			}
-			#top-strip>ul>li>button{
+			#navlinks>ul>li{
+				padding-left:3rem;
+			}
+			#navlinks>ul>li>button{
 				padding:1rem;
-				background-color:black;
 				color:white;
 				border:none;
-				border-radius:4rem;
+				font-size:1rem;
+				background:#472532;
+				border-bottom:2px solid rgb(1,1,1,0);
+				transition:0.3s;
 			}
-			#top-strip>ul>li>a{
+			#navlinks>ul>li>button:hover{
+				color:yellow;
+
+				border-bottom:2px solid rgb(120,120,120);
+				transition:0.3s;
+			}
+			#navlinks>ul>li>a{
 				text-decoration:none;
 				color:white;
 				padding:0.5rem;
-				background-color:red;
-				border-radius:5rem;
+				
+				
+			}
+			#logo>button{
+				padding:1.3rem;
+				background:black;
+				color:white;
+				width:20rem;
+				font-size:2rem;
+				border:none;
+				border-radius:3rem;
+				transition:0.3s;
+			}
+			#logo>button:hover{
+				background:white;
+				color:black;
+				transition:0.3s;
+			}
+			#hover{
+				display:grid;
+				widht:auto;
+				margin:auto;
+				grid-template-columns: auto auto auto;
+  				padding: 1rem;
+			}
+			#hover>div{
+				padding:1rem;
+
+			}
+			#hover>p>h2{
+				border:2px solid black;
+				border-radius:1rem;
+				background-color:black;
+				color:white;
+				font-size:1.2rem;
+				width:13rem;
 			}
 			#hover>table>tr>td>a{
-				padding:2rem;
 				text-decoration:none;
-				border:1 solid black;
-				color:white;
+				font-size:1.2rem;
+				color:black;
+			}
+			#hover>table>tr>td>a>button{
+				border:none;
+				border-radius:3rem;
+				padding:0.5rem;
+				width:6rem;
+				background-color:cornflowerblue;
+			}
+			#hover>div>h1{
 				
-			}
-			#hover>table{
-				background-color:#140014;
-			}
-			#hover>a{
-				width:auto;
+				color:black;
+				font-size:large;
 				padding:1rem;
-				margin:1rem;
-				display:flex;
-				justify-content:baseline;
-				text-decoration:none;
-				color:white;
-				background-color:#442323;
-				border-radius:1rem;
-				
 			}
-			#hover>table{
-				margin:2rem;
-				border:0.1rem solid black;
-				color:white;
-				display:flex;
-				width:auto;
-				justify-content:space-around;
+			#hover>div>a>button{
+				background:cornflowerblue;
+				padding:0.3rem;
+				border:none;
+				width:10rem;
+				border-radius:3rem;
 			}
+			#hover>div>a{
+				padding:1rem;
+			}
+			
 		</style>
 	</head>
-	<body>
+	<body onload="handle('suggest')">
 		<div class="container">
 			<div id="top-strip">
-				<ul>
-					<li><button type="button" id="suggest" onclick="handle('suggest')">Home-suggest</button></li>
-					<li><button type="button" id="products" onclick="handle('products')">Products category</button></li>
-					<li><button type="button" id="kart" onclick="handle('kart/show')">Your Kart</button></li>
-					<li><button type="button" id="order_hist" onclick="handle('order_hist')">Orders</button></li>
-					<li><a id="logout" href='/logout'>Logout</a></li>
-					<li><a id="profile" href='/profview'>Profile</a></li>
-				</ul>
+				<div id="logo">
+					<button type="button">Shop-ovo</button>
+				</div>
+				<div id="navlinks">
+					<ul>
+						<li><button type="button" id="suggest" onclick="handle('suggest')">Home-suggest</button></li>
+						<li><button type="button" id="products" onclick="handle('products')">Products category</button></li>
+						<li><button type="button" id="kart" onclick="handle('kart/show')">Your Kart</button></li>
+						<li><button type="button" id="order_hist" onclick="handle('order_hist')">Orders</button></li>
+						<li><a id="logout" href='/logout'><img height='50px' width='50px' src='src/refer.png'></a></li>
+						<li><a id="profile" href='/profview'><img height='50px' width='50px' src='src/user.png'></a></li>
+					</ul>
+				</div>
 			</div>
 			<div id="hover">
 
@@ -250,6 +340,61 @@ app.get('/profview',(req,res)=>{
 	}else{
 		res.redirect('/')
 	}
+})
+app.post('/update',(req,res)=>{
+	if (req.cookies.userdata!=null){
+		let name=req.body.uname.split(" ").join('')
+		con.query('update userdata set name='+"'"+name+"',"+"passwd="+"'"+req.body.passwd+"'"+' where email='+"'"+`${req.cookies.userdata.cookiemail}`+"'",(err,result)=>{
+			if (err) throw err
+		})
+		con.query('alter table kart_'+req.cookies.userdata.cookiename+' rename to kart_'+name,(err,est)=>{
+			if (err) throw err
+		})
+		con.query('alter table ord_'+req.cookies.userdata.cookiename+' rename to ord_'+name,(err,est)=>{
+			if (err) throw err
+		})
+		let user={
+			cookiename:name,
+			cookiemail:`${req.cookies.userdata.cookiemail}`,
+			cookiepass:req.body.passwd
+		}
+		res.cookie("userdata",user)
+		res.redirect('/home')
+	}else{
+		res.redirect('/')
+	}
+})
+app.post('/invoice',(req,res)=>{
+	if (req.cookies.userdata!=null){
+	const doc = new PDFDocument
+	doc.pipe(fs.createWriteStream('public/src/'+req.cookies.userdata.cookiemail+"_"+req.body.data+'.pdf'))
+	const filepath=__dirname+'/public/src'
+	con.query('select * from ord_'+req.cookies.userdata.cookiename+' where orderid='+"'"+req.body.data+"'",(err,result)=>{
+		if (err) throw err
+		doc.text('Shop-ovo.com',{
+			height:100,
+			width:100
+		})
+		doc.text('Thank you for shopping with us, we always try our best to deliver best products to your door')
+		doc.text('Customer : '+req.cookies.userdata.cookiename)
+		doc.text('Email : '+req.cookies.userdata.cookiemail)
+		doc.text('Product name : '+result[0].ordername)
+		doc.text('Product id : '+result[0].orderid)
+		doc.text('Price : '+result[0].ocost+"/-")
+		doc.text('Shipping Address : '+result[0].shipping)
+		doc.text('Contact no : ')
+		doc.text('Copyright : shop-ovo@2022')
+		doc.end()
+		res.type('application/pdf')
+		res.download(filepath+'/'+req.cookies.userdata.cookiemail+"_"+req.body.data+'.pdf',(errlog)=>{
+			if (errlog) throw errlog
+		})
+	})
+	
+	}else{
+		res.redirect("/")
+	}
+
 })
 app.post('/reset/:ret',(req,res)=>{
 	let opt = req.params.ret
@@ -279,7 +424,7 @@ app.post('/suggest',(req,res)=>{
 	console.log(req.cookies)
 	res.status=200
 	res.type('application/json')
-	con.query('SELECT pname,pcode from productlist',(err,result)=>{
+	con.query('SELECT pname,pcode,imgpath from productlist',(err,result)=>{
 		if (err) throw err
 		res.send(JSON.stringify(result))
 	})
@@ -308,6 +453,11 @@ app.get('/desc',(req,res)=>{
 						for (const c of cons){
 							let par=document.getElementById('details')
 							let chl=document.createElement('p')
+							let icon=document.createElement('img')
+							icon.setAttribute('src',c.imgpath)
+							icon.setAttribute('height','400px')
+							icon.setAttribute('width','400px')
+							par.appendChild(icon)
 							chl.innerHTML="Name : "+c.pname+"<br>"+"Price : "+c.pcost+"<br>"+"Description : "+c.description
 							par.appendChild(chl)
 						}
@@ -345,7 +495,8 @@ app.get('/desc',(req,res)=>{
 					.then(cons=>{
 						let bod={
 							co:'${req.query.pid}',
-							ord:cons[0].pname
+							ord:cons[0].pname,
+							imgp:cons[0].imgpath
 						}
 						fetch('/kart/addk',{
 							method:'post',
@@ -516,7 +667,7 @@ app.post('/sign_up',(req,res)=>{
 		}
 	res.cookie("userdata",user)
 	res.render('welcome',{
-		name:req.cookies.userdata.cookiename,
+		name:`${uname}`,
 		url:"/home"
 	})
 
